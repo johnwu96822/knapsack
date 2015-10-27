ed053c610ecb74302f19cc735fcc3998d9daa0f2
Harish Reddy <harishreddyreddy@gmail.com>
2015-10-27 11:43:27 -0700

    Allow space spserated list of tests to be passed in as patterns of tests to be run

--- lib/knapsack/distributors/base_distributor.rb
+++ lib/knapsack/distributors/base_distributor.rb
@@ -37,7 +37,7 @@ module Knapsack
       end
 
       def all_tests
-        @all_tests ||= Dir[test_file_pattern].sort
+        @all_tests ||= test_file_pattern.split(" ").sort
       end
 
       protected
