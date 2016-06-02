0a6d2ca7b8800e2326b4c0be8d383d0681407ce1
Harish Reddy <harishreddyreddy@gmail.com>
2016-06-02 16:50:54 -0700

    Dont run all default rspec pattern when there are no tests to run

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -12,7 +12,12 @@ module Knapsack
         puts allocator.leftover_node_tests
         puts
 
-      cmd = %Q[bundle exec rspec -r turnip/rspec -r turnip/capybara #{args}  #{allocator.stringify_node_tests}]
+        if allocator.stringify_node_tests.empty?
+          cmd = 'true'
+          puts 'No tests to run, check knapsack_all_tests_file_names'
+        else
+          cmd = %Q[bundle exec rspec -r turnip/rspec -r turnip/capybara #{args}  #{allocator.stringify_node_tests}]
+        end
 
         system(cmd)
         exit($?.exitstatus)
