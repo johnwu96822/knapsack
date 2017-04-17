3c7e14c2cb0b996de162b4e406160f8d8a570a09
John Wu <johnwu96822@gmail.com>
2017-04-17 15:15:37 -0700

    Test parallelization
    -Determine no of proccesses to use based on number of files
    -Fix concurrent rspec mysql problem
    -Skip IE
    -Use original order when distributing files

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -12,15 +12,51 @@ module Knapsack
         puts allocator.leftover_node_tests
         puts
 
+        num = max_process_count
+        if num > 1 && !skip_parallel?
+          test_slices = allocator.distribute_files(num)
+          if test_slices.length > 1
+            begin
+              puts "Tests will be parallelized into #{test_slices.length} processes"
+              Knapsack::Parallelizer::RSpecParallelizer.run(test_slices, args: args)
+              exit(0)
+            rescue => e
+              puts e.message
+              puts e.backtrace.join("\n\t")
+              exit(1)
+            end
+          end
+        end
         if allocator.stringify_node_tests.empty?
           cmd = 'true'
           puts 'No tests to run, check knapsack_all_tests_file_names'
         else
           cmd = %Q[bundle exec rspec -r turnip/rspec -r turnip/capybara #{args}  #{allocator.stringify_node_tests}]
         end
-
         system(cmd)
         exit($?.exitstatus)
+      end
+
+      # Number of CPUs for this machine
+      def self.ncpu
+        #sysctl for OSX, nproc for linux
+        num = RUBY_PLATFORM.include?('darwin') ? `sysctl -n hw.ncpu`.to_i : `nproc`.to_i
+        num <= 1 ? 1 : num
+      rescue Errno::ENOENT
+        1
+      end
+
+      # Maximum number of processes allowed for this agent
+      def self.max_process_count
+        # Default is 2 agents per instance
+        default_agent_count = 2
+        num_agents = (ENV['NUM_AGENTS_PER_INSTANCE'] || default_agent_count).to_i
+        num = (ncpu.to_f / (num_agents == 0 ? default_agent_count : num_agents)).ceil
+        num <= 1 ? 1 : num
+      end
+
+      def self.skip_parallel?
+        ENV['JS_DRIVER'] == 'selenium-ie-remote' || Knapsack::Util.to_bool(ENV['SKIP_PARALLEL'])
       end
     end
   end
