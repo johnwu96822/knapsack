f86ac73731a373e35965bfbdf5410474a829b7c3
John Wu <johnwu96822@gmail.com>
2017-04-26 14:54:23 -0700

    m

--- lib/knapsack/parallelizers/rspec_parallelizer.rb
+++ lib/knapsack/parallelizers/rspec_parallelizer.rb
@@ -1,6 +1,28 @@
 module Knapsack::Parallelizer
-  class RSpecParallelizer < Knapsack::Parallelizer::Base
+  class RSpecParallelizer
     class << self
+      # The first fork process will use the same resources as the single process,
+      # including the database name and the failure log file. Other forks will
+      # use resources with a process identifier plus an index (starting with 1) to
+      # distinguish their database names and failure log files. The failure log
+      # files will be combined at the end.
+      def run(test_slices, options = {})
+        # The first process will use the database name in the database.yml file.
+        # Other processes will use the database name appended with this identifier plus
+        # an index, starting with 1, 2, 3...
+        identifier = "_#{Process.pid}_"
+
+        forks = test_slices.length
+        setup(forks, identifier, options)
+
+        run_tests(test_slices, identifier, options)
+      rescue => e
+        puts e.message
+        puts e.backtrace.join("\n\t")
+      ensure
+        clean_up(forks, identifier, options)
+      end
+
       def test(test_slices, index, identifier, options)
         #sleep(index * 12)
         # For processes other than the very first one, fork_identifier is used
@@ -11,10 +33,10 @@ module Knapsack::Parallelizer
         # it locally, it would not overwrite the previous log files
         log_file = "knapsack#{options[:time].to_i}_#{index}.log"
         run_cmd("#{'TC_PARALLEL_ID='+fork_identifier if index > 0} bundle exec rspec -r turnip/rspec -r turnip/capybara #{options[:args]} #{test_slices[index].join(' ')} > #{log_file}")
-      ensure
         puts '**********************************'
         puts "Parallel testing #{index}/#{test_slices.length} finished"
         system("cat #{log_file}")
+        system("rm #{log_file}")
       end
 
       # Duplicating test databases for forks other than the first one, since the first fork uses
@@ -37,8 +59,34 @@ module Knapsack::Parallelizer
       end
 
       def clean_up(num, identifier, options = {})
+        output_logs(num, options[:time].to_i)
         combine_failures(num, identifier)
         clean_up_dbs(num, identifier)
+      end
+
+      protected
+
+      # test_slices is an array of filename arrays. Each filename list is to be
+      # run parallely by a process.
+      # identifier is the current process's identifier that will be used to further
+      # distinguish each fork's database name.
+      def run_tests(test_slices, identifier, options = {})
+        forks = test_slices.length
+        options[:time] = Time.now
+        if forks > 1
+          pids = []
+          forks.times do |i|
+            pids << fork do
+              test(test_slices, i, identifier, options)
+              # Force the fork to end without running at_exit bindings
+              Kernel.exit!
+            end
+          end
+          # Wait for the forks to finish the tests
+          pids.each {|pid| Process.wait(pid)}
+        else
+          test(test_slices, 0, identifier, options)
+        end
       end
 
       private
@@ -77,6 +125,21 @@ module Knapsack::Parallelizer
             if File.exist?(from)
               run_cmd("cat #{from} >> #{target}")
               # File.delete(from)
+            end
+          rescue => e
+            puts e.message
+            puts e.backtrace.join("\n\t")
+          end
+        end
+      end
+
+      def output_logs(num, timestamp)
+        num.times do |i|
+          log_file = "knapsack#{options[:time].to_i}_#{i}.log"
+          begin
+            if File.exist?(log_file)
+              system("cat #{log_file}")
+              system("rm #{log_file}")
             end
           rescue => e
             puts e.message
