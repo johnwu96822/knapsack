61d44025b4562ec6f9d7186519c28ec0ef54e638
John Wu <johnwu96822@gmail.com>
2017-05-01 13:31:29 -0700

    Test

--- lib/knapsack/parallelizers/rspec_parallelizer.rb
+++ lib/knapsack/parallelizers/rspec_parallelizer.rb
@@ -34,7 +34,7 @@ module Knapsack::Parallelizer
         # it locally, it would not overwrite the previous log files
         log_file = "tmp/knapsack_#{options[:time].to_i}_#{index}.log"
         run_cmd("#{'TC_PARALLEL_ID='+fork_identifier if index > 0} bundle exec rspec -r turnip/rspec -r turnip/capybara #{options[:args]} #{test_slices[index].join(' ')} > #{log_file}")
-        puts "******* Parallel testing #{index}/#{test_slices.length} finished ********"
+        puts "******* Parallel testing #{index + 1}/#{test_slices.length} finished ********"
         system("cat #{log_file}")
         system("rm #{log_file}")
       end
