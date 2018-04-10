e99113b672cec24a0987e4b1aa4b76282ac6a441
John Wu <johnwu96822@gmail.com>
2018-04-10 15:49:48 -0700

    Beluga for single process

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -31,9 +31,13 @@ module Knapsack
           cmd = 'true'
           puts 'No tests to run, check knapsack_all_tests_file_names'
         else
-          cmd = %Q[bundle exec rspec -r turnip/rspec -r turnip/capybara #{args}  #{allocator.stringify_node_tests}]
+          cmd = if ENV['ENABLE_BELUGA'] == 'true' && ENV['JS_DRIVER'] == 'selenium-chrome' &&
+            %Q[beluga turnip #{args} #{allocator.stringify_node_tests}]
+          else
+            %Q[bundle exec rspec -r turnip/rspec -r turnip/capybara #{args}  #{allocator.stringify_node_tests}]
+          end
         end
-        system(cmd)
+        Knapsack::Util.run_cmd(cmd)
         exit($?.exitstatus)
       end
 
